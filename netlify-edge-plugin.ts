b62b56ae9c5a5d82b716997e4dd9b06ed3043452
ascorbic <ascorbic@gmail.com>
2024-02-16 09:08:31 +0000

    Plugin refactor

--- netlify-edge-plugin.ts
+++ netlify-edge-plugin.ts
@@ -1,14 +1,31 @@
-import type { Plugin } from "vite";
+import type { Plugin, ResolvedConfig } from "vite";
+import { writeFile } from "node:fs/promises";
 import path from "path";
 
+function generateEntrypoint(server: string) {
+  return /* js */ `
+    export { default } from "${server}";
+
+    export const config = {
+      cache: "manual",
+      path: "/*",
+      excludedPath: ["/build/*", "/favicon.ico"],
+    };`;
+}
+
 export function netlifyEdgePlugin(): Plugin {
+  let resolvedConfig: ResolvedConfig;
   let currentCommand: string;
+  let isSsr: boolean | undefined;
   return {
     name: "vite-plugin-remix-netlify-edge",
     config(config, { command, isSsrBuild }) {
       currentCommand = command;
+      isSsr = isSsrBuild;
       // Configure for edge functions
       if (command === "build" && isSsrBuild) {
+        config.build ||= {};
+        config.build.manifest = true;
         const server = path.resolve(config.root ?? "", "server.ts");
         config.ssr = {
           ...config.ssr,
@@ -31,6 +48,9 @@ export function netlifyEdgePlugin(): Plugin {
           }
         }
       }
+    },
+    async configResolved(config) {
+      resolvedConfig = config;
     },
     async resolveId(source, importer, options) {
       // Conditionally resolve the server entry based on the command.
@@ -55,6 +75,20 @@ export function netlifyEdgePlugin(): Plugin {
         }
       }
       return null;
+    },
+    async writeBundle(config, bundle) {
+      if (currentCommand === "build" && isSsr) {
+        await writeFile(
+          path.join(
+            resolvedConfig.root,
+            ".netlify/edge-functions",
+            "server.mjs"
+          ),
+          generateEntrypoint(
+            path.join(resolvedConfig.build.outDir, "server.js")
+          )
+        );
+      }
     },
   };
 }
--- vite.config-edge.ts
+++ vite.config-edge.ts
@@ -2,37 +2,7 @@ import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { netlifyEdgePlugin } from "./netlify-edge-plugin";
-import { posix as path } from "node:path";
-import { writeFile, mkdir } from "node:fs/promises";
-import { ResolvedVitePluginConfig } from "@remix-run/dev/dist/vite/plugin";
-
-function generateEntrypoint(config: ResolvedVitePluginConfig) {
-  const server = path.resolve(config.buildDirectory, "server", "server.js");
-  return /* js */ `
-    export { default } from "${server}";
-
-    export const config = {
-      cache: "manual",
-      path: "/*",
-      excludedPath: ["/build/*", "/favicon.ico"],
-    };`;
-}
 
 export default defineConfig({
-  plugins: [
-    remix({
-      buildEnd: async ({ remixConfig }) => {
-        const entrypoint = generateEntrypoint(remixConfig);
-        await mkdir(path.resolve(".netlify/edge-functions"), {
-          recursive: true,
-        });
-        await writeFile(
-          path.resolve(".netlify", "edge-functions", "server.mjs"),
-          entrypoint
-        );
-      },
-    }),
-    netlifyEdgePlugin(),
-    tsconfigPaths(),
-  ],
+  plugins: [remix(), netlifyEdgePlugin(), tsconfigPaths()],
 });
